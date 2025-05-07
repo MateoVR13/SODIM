705fe716f6bb7a1ba019c3e6d2c55b94ed55b921
MateoVR13 <mattvr130@gmail.com>
2025-05-06 19:06:44 -0500

    Updated order_status estimated.delivery.date

--- sodim/core/urls.py
+++ sodim/core/urls.py
@@ -1,19 +1,48 @@
 from django.urls import path
-from .views import CustomLoginView, CustomLogoutView, EPSDashboardView, DoctorDashboardView, DoctorListView, DoctorCreateView, DoctorUpdateView, DoctorDeleteView, EPSMedicationStockListView, EPSMedicationStockCreateView, PrescriptionListView, PrescriptionCreateView, OrderStatus, LandingPage
+from .views import (
+    CustomLoginView,
+    CustomLogoutView,
+    EPSDashboardView,
+    DoctorDashboardView,
+    DoctorListView,
+    DoctorCreateView,
+    DoctorUpdateView,
+    DoctorDeleteView,
+    EPSMedicationStockListView,
+    EPSMedicationStockCreateView,
+    PrescriptionListView,
+    PrescriptionCreateView,
+    OrderStatus,
+    LandingPage,
+)
 
 urlpatterns = [
-    path('', LandingPage, name='landing_page'),
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
-    path('eps/dashboard/', EPSDashboardView.as_view(), name='eps_dashboard'),
-    path('doctor/dashboard/', DoctorDashboardView.as_view(), name='doctor_dashboard'),
-    path('eps/doctors/', DoctorListView.as_view(), name='doctor_list'),
-    path('eps/doctors/create/', DoctorCreateView.as_view(), name='doctor_create'),
-    path('eps/doctors/<int:pk>/edit/', DoctorUpdateView.as_view(), name='doctor_edit'),
-    path('eps/doctors/<int:pk>/delete/', DoctorDeleteView.as_view(), name='doctor_delete'),
-    path('eps/medications/', EPSMedicationStockListView.as_view(), name='medication_stock_list'),
-    path('eps/medications/add/', EPSMedicationStockCreateView.as_view(), name='medication_stock_add'),
-    path('prescriptions/', PrescriptionListView.as_view(), name='prescription_list'),
-    path('prescriptions/create/', PrescriptionCreateView.as_view(), name='prescription_create'),
-    path('order/status/', OrderStatus, name='order_status'),
+    path("", LandingPage, name="landing_page"),
+    path("login/", CustomLoginView.as_view(), name="login"),
+    path("logout/", CustomLogoutView.as_view(), name="logout"),
+    path("eps/dashboard/", EPSDashboardView.as_view(), name="eps_dashboard"),
+    path("doctor/dashboard/", DoctorDashboardView.as_view(), name="doctor_dashboard"),
+    path("eps/doctors/", DoctorListView.as_view(), name="doctor_list"),
+    path("eps/doctors/create/", DoctorCreateView.as_view(), name="doctor_create"),
+    path("eps/doctors/<int:pk>/edit/", DoctorUpdateView.as_view(), name="doctor_edit"),
+    path(
+        "eps/doctors/<int:pk>/delete/", DoctorDeleteView.as_view(), name="doctor_delete"
+    ),
+    path(
+        "eps/medications/",
+        EPSMedicationStockListView.as_view(),
+        name="medication_stock_list",
+    ),
+    path(
+        "eps/medications/add/",
+        EPSMedicationStockCreateView.as_view(),
+        name="medication_stock_add",
+    ),
+    path("prescriptions/", PrescriptionListView.as_view(), name="prescription_list"),
+    path(
+        "prescriptions/create/",
+        PrescriptionCreateView.as_view(),
+        name="prescription_create",
+    ),
+    path("order/status/", OrderStatus, name="order_status"),
 ]
